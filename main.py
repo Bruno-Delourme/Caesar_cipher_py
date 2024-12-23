a60b3f4cf5acf8d425eb4cdddb5e71f58b92e039
Bruno-Delourme <delourmebruno@gmail.com>
2024-12-23 19:59:39 +0100

    chiffrement et dechiffrement possible

--- main.py
+++ main.py
@@ -1,26 +1,21 @@
-text = 'Cipher Caesar'
-shift = 1
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-shifted_text = '' 
-i = 0  
-
-# Boucle 
-while i < len(text):
-    char = text[i]  # le caractere actuel
-
-    if char.lower() in alphabet:  # Si c'est une lettre
-
-        # Trouve l'index et decale avec alphabet.index()
-        shifted_char = alphabet[(alphabet.index(char.lower()) + shift) % len(alphabet)]
-
-        # Respect de la casse
-        if char.isupper():
-            shifted_text += shifted_char.upper()
+def caesar_cipher(text, shift, decrypt=False):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    shifted_text = ''
+    
+    if decrypt:
+        shift = -shift
+    
+    for char in text:
+        if char.lower() in alphabet:
+            shifted_char = alphabet[(alphabet.index(char.lower()) + shift) % len(alphabet)]
+            shifted_text += shifted_char.upper() if char.isupper() else shifted_char
         else:
-            shifted_text += shifted_char
-    else:
-        # Si c'est pas une lettre
-        shifted_text += char
-    i += 1  # Alors on passe au caractère suivant
+            shifted_text += char
+            
+    return shifted_text
 
-print(shifted_text)
+texte = 'Cipher Caesar'
+texte_chiffre = caesar_cipher(texte, 1)
+print(f"Texte chiffré: {texte_chiffre}")
+texte_dechiffre = caesar_cipher(texte_chiffre, 1, decrypt=True)
+print(f"Texte déchiffré: {texte_dechiffre}")
